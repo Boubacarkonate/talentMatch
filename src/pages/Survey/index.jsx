d9a0fb5db151cbad2bbaa8514c8589d6a610e30a
Bouba <boubacar.konate@outlook.fr>
2024-08-23 21:39:43 +0200

    connexion api

--- src/pages/Survey/index.jsx
+++ src/pages/Survey/index.jsx
@@ -1,10 +1,43 @@
-import { Link, Outlet, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import colors from '../../utils/style/colors';
+import { Loader } from '../../utils/style/Atoms';
+import { useEffect, useState } from 'react';
+
+const SurveyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const QuestionTitle = styled.h2`
+  text-decoration: underline;
+  text-decoration-color: ${colors.primary};
+`;
+
+const QuestionContent = styled.span`
+  margin: 30px;
+`;
+
+const LinkWrapper = styled.div`
+  padding-top: 30px;
+  & a {
+    color: black;
+  }
+  & a:first-of-type {
+    margin-right: 20px;
+  }
+`;
 
 function Survey() {
   const { questionNumber } = useParams();
   const questionNumberInt = parseInt(questionNumber);
-  const prevQuestion = questionNumberInt === 1 ? 1 : questionNumberInt - 1;
-  const nextQuestion = questionNumberInt + 1;
+  const prevQuestionNumber =
+    questionNumberInt === 1 ? 1 : questionNumberInt - 1;
+  const nextQuestionNumber = questionNumberInt + 1;
+  const [surveyData, setSurveyData] = useState({});
+  // const [isDataLoading, setDataLoading] = useState(false);   pour useEffect avec promise
+  const [isDataLoading, setDataLoading] = useState(false);
 
   // const Ternaire =
   //   questionNumberInt === 10 ? (
@@ -13,27 +46,51 @@ function Survey() {
   //     <Link to={`/survey/${nextQuestion}`}>Suivante</Link>
   //   );
 
+  async function fetchData() {
+    setDataLoading(true);
+    try {
+      const response = await fetch(`http://localhost:8000/survey`);
+      const { surveyData } = await response.json();
+      setSurveyData(surveyData);
+    } catch (error) {
+      console.log('===== error =====', error);
+      // setError(true);
+    } finally {
+      setDataLoading(false); // Stop loading after fetching data
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   setDataLoading(true);
+  //   fetch(`http://localhost:8000/survey`).then((response) =>
+  //     response.json().then(({ surveyData }) => {
+  //       setSurveyData(surveyData);
+  //       setDataLoading(false);
+  //     }),
+  //   );
+  // }, []);
+
   return (
-    <div>
-      <h1 className=" font-bold underline">Questionnaire 🧮</h1>
-      <h2>
-        Question <span className="font-black">{questionNumber}</span>{' '}
-      </h2>
-      <div>
-        <Link to={`/survey/${prevQuestion}`}>Précédent</Link>
-        {questionNumberInt === 10 ? (
-          <Link to="/results">Résultat</Link>
+    <SurveyContainer>
+      <QuestionTitle>Question {questionNumber}</QuestionTitle>
+      {isDataLoading ? (
+        <Loader />
+      ) : (
+        <QuestionContent>{surveyData[questionNumber]}</QuestionContent>
+      )}
+      <LinkWrapper>
+        <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+        {surveyData[questionNumberInt + 1] ? (
+          <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
         ) : (
-          <Link to={`/survey/${nextQuestion}`}>Suivante</Link>
+          <Link to="/results">Résultats</Link>
         )}
-
-        {/* {Ternaire} */}
-
-        {/* <Link to={ClientForm}>Questionnaire Client</Link>
-      <Link to={FreeLanceForm}>Questionnaire FreeLance</Link> */}
-        <Outlet />
-      </div>
-    </div>
+      </LinkWrapper>
+    </SurveyContainer>
   );
 }
 
